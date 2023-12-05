b8e67b092002d27b2ab31049c369aab281c8ba9e
mjspano3 <129989363+mjspano3@users.noreply.github.com>
2023-12-05 09:21:59 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,77 +4,35 @@
 #include <linux/kernel.h>   
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
-#define BUFSIZE  100
 
 
-MODULE_LICENSE("Dual BSD/GPL");
-MODULE_AUTHOR("Liran B.H");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Tripp Spano");
 
-static int irq=20;
-module_param(irq,int,0660);
-
-static int mode=1;
-module_param(mode,int,0660);
 
 static struct proc_dir_entry *ent;
 
 static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos) 
 {
 	
-	// int num,c,i,m;
-	// char buf[BUFSIZE];
-	// if(*ppos > 0 || count > BUFSIZE)
-	// 	return -EFAULT;
-	// if(copy_from_user(buf, ubuf, count))
-	// 	return -EFAULT;
-	// num = sscanf(buf,"%d %d",&i,&m);
-	// if(num != 2)
-	// 	return -EFAULT;
-	// irq = i; 
-	// mode = m;
-	// c = strlen(buf);
-	// *ppos = c;
-	// return c;
-
-
-
-	
 	return count;
-}
-
-static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
-{
-	// char buf[BUFSIZE];
-	int len=0;
-	// if(*ppos > 0 || count < BUFSIZE)
-	// 	return 0;
-	// len += sprintf(buf,"irq = %d\n",irq);
-	// len += sprintf(buf + len,"mode = %d\n",mode);
-	
-	// if(copy_to_user(ubuf,buf,len))
-	// 	return -EFAULT;
-	// *ppos = len;
-	return len;
 }
 
 static struct file_operations myops = 
 {
 	.owner = THIS_MODULE,
-	.read = myread,
 	.write = mywrite,
 };
 
 static int simple_init(void)
 {
-	ent=proc_create("hello",0660,NULL,&myops);
-	printk(KERN_ALERT "hello...\n");
+	ent=proc_create("cs5204",0660,NULL,&myops);
 	return 0;
 }
 
 static void simple_cleanup(void)
 {
 	proc_remove(ent);
-	printk(KERN_WARNING "bye ...\n");
 }
 
 module_init(simple_init);
