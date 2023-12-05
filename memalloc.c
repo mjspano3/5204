197acb565707c69219361d9ed3636e4d28d7ad92
mjspano3 <129989363+mjspano3@users.noreply.github.com>
2023-12-05 09:34:39 -0500

    Create memalloc.c

---
+++ memalloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define GB_SIZE 1073741824
+#define CHUNK_SIZE 4096
+
+int main(){
+
+    void* ptr = malloc(GB_SIZE);
+
+    size_t total = GB_SIZE/CHUNK_SIZE;
+    size_t i;
+
+    for(i = 0; i < total; i++){
+        //function
+        char command[128];
+        sprintf(command, "echo %p > /proc/cs5204", (char*)ptr + i * CHUNK_SIZE);
+        system(command);
+        
+    }
+}
