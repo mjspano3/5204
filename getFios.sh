33650cc3a7ed12490b43591bd3e9d77d8fcfe93b
mjspano3 <129989363+mjspano3@users.noreply.github.com>
2023-12-09 00:27:31 -0500

    Create getFios.sh

---
+++ getFios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/mjspano3/5204/main/random_read.fio
+wget https://raw.githubusercontent.com/mjspano3/5204/main/random_write.fio
+wget https://raw.githubusercontent.com/mjspano3/5204/main/random_write_steady.fio
+wget https://raw.githubusercontent.com/mjspano3/5204/main/sequential_read.fio
+wget https://raw.githubusercontent.com/mjspano3/5204/main/sequential_write.fio
+wget https://raw.githubusercontent.com/mjspano3/5204/main/sequential_write_steady.fio
