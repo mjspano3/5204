689d1738b6e82204d53bf07e774b89264235a8b4
mjspano3 <129989363+mjspano3@users.noreply.github.com>
2023-12-09 00:36:23 -0500

    Update runBenchmarks.sh

--- runBenchmarks.sh
+++ runBenchmarks.sh
@@ -4,3 +4,5 @@ fio benchmarks/random_read.fio
 fio benchmarks/random_write.fio
 fio benchmarks/sequential_read.fio
 fio benchmarks/sequential_write.fio
+fio benchmarks/random_write_steady.fio
+fio benchmarks/sequential_write_steady.fio
